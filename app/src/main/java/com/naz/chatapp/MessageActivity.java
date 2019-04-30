559c9af45f53423fbe66088eaf4435471f29ab1e
Nazar <nazar.semenyuck@yandex.ua>
2019-04-30 13:15:12 +0200

    sending messages

--- app/src/main/java/com/naz/chatapp/MessageActivity.java
+++ app/src/main/java/com/naz/chatapp/MessageActivity.java
@@ -8,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.naz.chatapp.Model.User;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -28,6 +33,9 @@ public class MessageActivity extends AppCompatActivity {
 
     CircleImageView profile_image;
     TextView username;
+
+    ImageButton btn_send;
+    EditText txt_send;
 
     FirebaseUser firebaseUser;
     DatabaseReference reference;
@@ -53,13 +61,26 @@ public class MessageActivity extends AppCompatActivity {
 
         profile_image = findViewById(R.id.profile_image);
         username = findViewById(R.id.username);
+        btn_send = findViewById(R.id.btn_send);
+        txt_send = findViewById(R.id.text_send);
 
         intent = getIntent();
-
-        String userid = intent.getStringExtra("userid");
+        final String userid = intent.getStringExtra("userid");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = txt_send.getText().toString();
+                if (!msg.equals(""))
+                    sendMessage(firebaseUser.getUid(), userid, msg);
+                else
+                    Toast.makeText(MessageActivity.this, "you can't send empty message", Toast.LENGTH_SHORT).show();
+                txt_send.setText("");
+            }
+        });
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -78,5 +99,16 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
